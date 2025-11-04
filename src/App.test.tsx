91d2ca4b2a9e510eb623cb2d6f2227611b03120b
jphdcdcgi <james.pickering@cgi.com>
2025-11-04 15:55:42 +0000

    add Video Playlist

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 describe("App", () => {
   it("renders welcome text", () => {
     render(<App />);
-    expect(screen.getByText("Hello World!")).toBeInTheDocument();
+    expect(screen.getByText("Featured Playlist")).toBeInTheDocument();
   });
 });
